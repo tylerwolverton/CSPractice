4540cc0a85d1be45c7d2af96239b0449a7444b92
tylerwolverton <tylerwolvert@gmail.com>
2018-04-22 11:14:55 -0400

    Added a couple more cases

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -4,16 +4,17 @@
 #include <iostream>
 
 int main() {
-	std::vector<int> sortedArray {1, 2, 5, 7, 9, 13, 15, 21, 29, 32, 35};
+	std::vector<int> sortedArray{ 1, 2, 5, 7, 9, 13, 15, 21, 29, 32, 35 };
+	std::vector<int> notExistArray{ -3, 3, 60 };
 
 	// Even number of vals
 	for (auto val : sortedArray) {
 		assert(BinarySearch::Find(sortedArray, val) == true);
 	}
 
-	assert(BinarySearch::Find(sortedArray, -3) == false);
-	assert(BinarySearch::Find(sortedArray, 3) == false);
-	assert(BinarySearch::Find(sortedArray, 60) == false);
+	for (auto val : notExistArray) {
+		assert(BinarySearch::Find(sortedArray, val) == false);
+	}
 
 	// Odd number of vals
 	sortedArray.push_back(42);
@@ -21,12 +22,28 @@ int main() {
 		assert(BinarySearch::Find(sortedArray, val) == true);
 	}
 
-	assert(BinarySearch::Find(sortedArray, -3) == false);
-	assert(BinarySearch::Find(sortedArray, 3) == false);
-	assert(BinarySearch::Find(sortedArray, 60) == false);
+	for (auto val : notExistArray) {
+		assert(BinarySearch::Find(sortedArray, val) == false);
+	}
 
+	// Empty array
 	sortedArray.clear();
 	assert(BinarySearch::Find(sortedArray, 5) == false);
+
+	// 1 value
+	sortedArray.push_back(4);
+	assert(BinarySearch::Find(sortedArray, 4) == true);
+	for (auto val : notExistArray) {
+		assert(BinarySearch::Find(sortedArray, val) == false);
+	}
+
+	// 2 values
+	sortedArray.push_back(6);
+	assert(BinarySearch::Find(sortedArray, 4) == true);
+	assert(BinarySearch::Find(sortedArray, 6) == true);
+	for (auto val : notExistArray) {
+		assert(BinarySearch::Find(sortedArray, val) == false);
+	}
 
 	std::cout << "Woohoo! Passed!" << std::endl;
 	return 0;
